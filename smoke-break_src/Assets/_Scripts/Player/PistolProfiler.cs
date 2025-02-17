4e677bd8c1ccd31ceea032f7c7e300e8f0afc70d
johnshields <johndshields14@gmail.com>
2025-02-17 15:20:45 +0000

    Repo update.

--- smoke-break_src/Assets/_Scripts/Player/PistolProfiler.cs
+++ smoke-break_src/Assets/_Scripts/Player/PistolProfiler.cs
@@ -32,7 +32,7 @@ namespace _Scripts.Player
         [Header("Weapon Visibility")] [SerializeField]
         private GameObject pistol;
 
-        private const float WeaponHideTime = 5f;
+        private const float WeaponHideTime = 10f;
         private float _lastActionTime;
 
         [Header("Aiming Settings")] [SerializeField]
