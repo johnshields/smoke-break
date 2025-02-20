15df67ab847255b8e1e5b9f54168a06ea018d7ee
johnshields <johndshields14@gmail.com>
2025-02-20 11:05:26 +0000

    SaveManager: updated for testing.

--- smoke-break_src/Assets/_Scripts/Managers/InitSave.cs
+++ smoke-break_src/Assets/_Scripts/Managers/InitSave.cs
@@ -1,4 +1,3 @@
-using System;
 using _Scripts.Player;
 using UnityEngine;
 
@@ -6,14 +5,17 @@ namespace _Scripts.Managers
 {
     public class InitSave : MonoBehaviour
     {
-        public bool saveGame;
         private PlayerProfiler _playerProfiler;
         private PistolProfiler _pistolProfiler;
+        public bool saveGame;
+        public bool resetGame;
+        private PlayerRespawner _playerRespawner;
 
         private void Start()
         {
             _pistolProfiler = FindObjectOfType<PistolProfiler>();
             _playerProfiler = FindObjectOfType<PlayerProfiler>();
+            _playerRespawner = FindObjectOfType<PlayerRespawner>();
             SaveManager.SaveGame(_playerProfiler, _pistolProfiler);
         }
 
@@ -23,6 +25,12 @@ namespace _Scripts.Managers
             {
                 saveGame = false;
                 SaveManager.SaveGame(_playerProfiler, _pistolProfiler);
+            }
+            else if (resetGame)
+            {
+                resetGame = false;
+                SaveManager.ResetGame();
+                _playerRespawner.InitRespawn();
             }
         }
     }
