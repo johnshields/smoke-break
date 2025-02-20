ca06499360c5c8dcfb760f0c1522a4bd1dea46b7
johnshields <johndshields14@gmail.com>
2025-02-20 11:26:54 +0000

    FPS counter for testing.

--- smoke-break_src/Assets/_Scripts/Managers/FpsManager.cs
+++ smoke-break_src/Assets/_Scripts/Managers/FpsManager.cs
@@ -1,13 +1,24 @@
+using TMPro;
 using UnityEngine;
 
 namespace _Scripts.Managers
 {
     public class FpsManager : MonoBehaviour
     {
+        public TextMeshProUGUI fpsText;
+        private float _deltaTime;
+
         private void Start()
         {
             Application.targetFrameRate = 60;
             QualitySettings.vSyncCount = 0;
+        }
+
+        private void Update()
+        {
+            _deltaTime += (Time.deltaTime - _deltaTime) * 0.1f;
+            float fps = 1.0f / _deltaTime;
+            fpsText.text = $"FPS: {Mathf.Ceil(fps)}";
         }
     }
 }
